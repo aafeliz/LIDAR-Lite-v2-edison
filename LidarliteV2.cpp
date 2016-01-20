f220326a5476b1f0b0160e77bc71cbc719b6e2fb
aafeliz <arielfeliz@host-j-127.dhcp.stevens-tech.edu>
2016-01-19 19:00:22 -0500

    Working sample of using the lidar lite v2 on the intel edison.

---
+++ LidarliteV2.cpp
@@ -0,0 +1,73 @@
+/*#include "Lidar/Lidar.hpp"
+#include <iostream>
+
+LIDARLite lidar;
+int distance = 66;
+int main()
+{
+	lidar.begin();
+
+	while(1)
+	{
+		distance = lidar.distance();//(false, false, LIDARI2C_ADDRESS);
+		std::cout << distance << std::endl;
+		sleep(1);
+	}
+}
+*/
+
+
+#include <iostream>
+#include <stdio.h>
+#include <time.h>
+//#include <unistd.h>
+//#include <signal.h>
+//#include <err.h>
+//#include <stdbool.h>
+
+#include "mraa.hpp"
+
+#define GPIO14 36
+#define GPIO15 48
+#define HIGH 1
+#define LOW  0
+
+
+unsigned long pulse_width;
+unsigned long pTime,count;
+unsigned long cTime;
+int timeOut = 0;
+#define TIMEOUT 1000
+int main()
+{
+	mraa::Gpio* trigger_pin = NULL;
+	mraa::Gpio* monitor_pin = NULL;
+	trigger_pin = new mraa::Gpio(GPIO14);
+	monitor_pin = new mraa::Gpio(GPIO15);
+	trigger_pin->dir(mraa::DIR_OUT);
+	monitor_pin->dir(mraa::DIR_IN);
+	while(1)
+	{
+		count = 0;
+		pTime = clock();
+		if(monitor_pin->read() == HIGH)
+		{
+			while(monitor_pin->read() == HIGH)
+			{
+				//count++;
+			}
+			cTime = clock();
+			pulse_width = cTime - pTime;//count;
+			if(pulse_width != 0)
+			{
+				pulse_width = pulse_width/10;
+				std::cout << pulse_width << std::endl;
+			}
+		}
+
+
+		sleep(0.5);
+
+	}
+}
+
